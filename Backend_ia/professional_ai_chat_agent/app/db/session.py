e9142bac500a017c734944d7aa44737a96c6589d
JuanC1824 <juanjosecardenas142006@gmail.com>
2026-04-10 08:38:50 -0500

    Agrego session

--- Backend_ia/professional_ai_chat_agent/app/db/session.py
+++ Backend_ia/professional_ai_chat_agent/app/db/session.py
@@ -5,17 +5,12 @@ from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
 
-DATABASE_URL = f"mysql+pymysql://avnadmin:{os.getenv('PASSWORD_BD')}@reporteloya-davyz2006-bbb8.k.aivencloud.com:11438/defaultdb"
+DATABASE_URL = f"mysql+pymysql://avnadmin:{os.getenv('PASSWORD_BD')}@reporteloya-davyz2006-bbb8.k.aivencloud.com:11438/defaultdb?ssl_disabled=true"
 
 engine = create_engine(
     DATABASE_URL,
     pool_pre_ping=True,
-    echo=True,
-    connect_args={
-        "ssl": {
-            "ca": "/etc/ssl/certs/ca-certificates.crt"
-        }
-    }
+    echo=True   
 )
 
 SessionLocal = sessionmaker(
